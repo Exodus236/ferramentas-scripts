0c270d3a9ea7f729a1d389df51e34c30752251f3
Exodus236 <69881757+Exodus236@users.noreply.github.com>
2021-06-17 00:12:42 -0400

    Update script.py

--- Requests/script.py
+++ Requests/script.py
@@ -1,1 +1,21 @@
 
+#!/usr/bin/env python3
+import requests
+import time
+import os
+
+def requi():
+    alvo=input("alvo: ")
+    arquivo=open("wordlist.txt", "r")
+    for cada in arquivo:
+        request=alvo + cada
+        requisicao=requests.get(request)
+        print(requisicao.status_code)
+        if requisicao.status_code == 404:
+            print("Off -> :( ")
+        elif requisicao.status_code == 200:
+            print("Diretorio vivo -> :) !!!!")
+        else:
+            print("Nao Entendido")
+
+requi()
