8bd9dea7dca7773abd38fd73650fcf45f94c800f
Exodus236 <69881757+Exodus236@users.noreply.github.com>
2021-06-17 00:33:48 -0400

    Update server-ssh.py

--- Servidor-ssh/server-ssh.py
+++ Servidor-ssh/server-ssh.py
@@ -1,1 +1,36 @@
 
+#!/usr/bin/env python3 
+ 
+import time
+import sys
+import os 
+import requests 
+import paramiko
+from paramiko import SSHClient
+import getpass
+
+
+def ssh():
+    alvo=input("passe o ip do SSH: ")
+    usuario=input("Passe o usuario: ")
+    senha=getpass.getpass("Passe a senha: ")
+    porta=input("Passe a porta: ")
+    try:
+        ssh=paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=f"{alvo}",port=f"{porta}",username=f"{usuario}",password=f"{senha}")
+        cmd=input("Passe o comando $: ")
+        stdin, stdout, stderr = ssh.exec_command(f'{cmd} \n')
+        print(stdout.readlines())
+    except paramiko.ssh_exception.AuthenticationException:
+        print("Erro de autenticacao")
+        time.sleep(0.5)
+        print("Tentando se reconectar")
+        ssh.connect(hostname=f"{alvo}",username=f"{usuario}",password=f"{senha}")
+        cmd=input("Passe comando $: ")
+        stdin, stdout, stderr = ssh.exec_command(f"{cmd} \n")
+        stdin.write('y/n')
+        stdin.flush()
+        print(stdout.readlines())
+
+ssh()
